f4a8d5c8bebee3d108f4dfc914141bde031c460b
kmtamu <kmr.manish@tamu.edu>
2016-11-12 20:14:44 -0600

    dynamic

--- WalkIn/views.py
+++ WalkIn/views.py
@@ -8,11 +8,20 @@ from .models import *
 
 api_key = 45717642
 api_secret = "df1752d514a1e6d89f98a66bb661a08c2db8ee73"
-opentok = OpenTok(api_key, api_secret) // \\
+opentok = OpenTok(api_key, api_secret)
+session = opentok.create_session()
+#create session ID from the created session above
+session_id = session.session_id
+#Now generate the token for this session
+token=session.generate_token()
+
 
 def index(request):
     template = loader.get_template('index.html')
     context ={ #Has all the data for index.html view to render to the user
-        'var': "Hello"
+        'sessionID' : session_id,
+        'token' : token,
+        'Api_key' : api_key
+
     }
     return HttpResponse(template.render(context, request))
